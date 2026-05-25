cb6000a01225200681d0dc9acfafb8261a47ecda
Austin Bowen <austin.bowen.314@gmail.com>
2026-05-25 00:55:46 -0400

    Add train.py stub

---
+++ src/asg/train.py
@@ -0,0 +1,33 @@
+import argparse
+
+from asg.datasets import load_animal_sounds_dataset
+
+
+def main() -> None:
+    args = parse_args()
+    print(args)
+
+    dataset = load_animal_sounds_dataset(args.dataset)
+    print(dataset)
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--dataset",
+        choices=["esc50"],
+        required=True,
+    )
+
+    parser.add_argument(
+        "--model",
+        choices=["model0"],
+        required=True,
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
